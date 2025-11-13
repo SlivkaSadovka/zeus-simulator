3f666870ed93f08b14edf76928f8745eccb8f383
marukatze <kaffeekoenigg@gmail.com>
2025-11-13 10:24:28 +0300

    added more methods to requesting APIs

--- src/main/java/org/zeus/demo/service/PersonalityRandomGeneratorService.java
+++ src/main/java/org/zeus/demo/service/PersonalityRandomGeneratorService.java
@@ -49,4 +49,57 @@ public class PersonalityRandomGeneratorService {
             return "DefaultLast";
         }
     }
+
+    public static String generateFavoriteJoke() {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://api.chucknorris.io/jokes/random"))
+                    .GET()
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String body = response.body();
+            int start = body.indexOf("\"value\":\"") + 9;
+            int end = body.indexOf("\"", start);
+            return body.substring(start, end);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return "No joke available";
+        }
+    }
+
+    public static String generateFavoriteQuote() {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://api.quotable.io/random"))
+                    .GET()
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String body = response.body();
+            int start = body.indexOf("\"content\":\"") + 11;
+            int end = body.indexOf("\"", start);
+            return body.substring(start, end);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return "No quote available";
+        }
+    }
+
+    public static int generateLuckyNumber() {
+        try {
+            // Запрашиваем 1 случайное число от 1 до 100
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain&rnd=new"))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String body = response.body().trim(); // API возвращает plain text с числом
+            return Integer.parseInt(body);
+
+        } catch (IOException | InterruptedException | NumberFormatException e) {
+            e.printStackTrace();
+            return -1; // на случай ошибки
+        }
+    }
+
 }
