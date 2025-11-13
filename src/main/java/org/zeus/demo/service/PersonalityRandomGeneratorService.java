088ac22e7a09839cb15dc0d40bc3078972a52010
marukatze <kaffeekoenigg@gmail.com>
2025-11-13 10:14:50 +0300

    reworked generation to avoid unnecessary dependence

--- src/main/java/org/zeus/demo/service/PersonalityRandomGeneratorService.java
+++ src/main/java/org/zeus/demo/service/PersonalityRandomGeneratorService.java
@@ -1,7 +1,5 @@
 package org.zeus.demo.service;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -9,43 +7,46 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Random;
 
 @Service
 public class PersonalityRandomGeneratorService {
 
-    private static final String API_URL = "https://randomuser.me/api/";
-
     private static final HttpClient client = HttpClient.newHttpClient();
+    private static final Random random = new Random();
 
     public static String generateFirstName() {
         try {
-            JSONObject name = fetchRandomName();
-            return name.getString("first");
-        } catch (Exception e) {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://randomuser.me/api/"))
+                    .GET()
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            // Парсим имя из JSON вручную, без библиотек:
+            String body = response.body();
+            int start = body.indexOf("\"first\":\"") + 9;
+            int end = body.indexOf("\"", start);
+            return body.substring(start, end);
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
-            return "Unknown";
+            return "DefaultFirst";
         }
     }
 
     public static String generateLastName() {
         try {
-            JSONObject name = fetchRandomName();
-            return name.getString("last");
-        } catch (Exception e) {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://randomuser.me/api/"))
+                    .GET()
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String body = response.body();
+            int start = body.indexOf("\"last\":\"") + 8;
+            int end = body.indexOf("\"", start);
+            return body.substring(start, end);
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
-            return "Unknown";
+            return "DefaultLast";
         }
-    }
-
-    private static JSONObject fetchRandomName() throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(API_URL))
-                .GET()
-                .build();
-
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        JSONObject json = new JSONObject(response.body());
-        JSONArray results = json.getJSONArray("results");
-        return results.getJSONObject(0).getJSONObject("name");
     }
 }
