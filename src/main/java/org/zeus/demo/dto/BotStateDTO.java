4ea82f46b73ac3f6c04eb41d5d0a297d097748ec
marukatze <kaffeekoenigg@gmail.com>
2025-12-04 20:03:58 +0300

    front demo

--- src/main/java/org/zeus/demo/dto/BotStateDTO.java
+++ src/main/java/org/zeus/demo/dto/BotStateDTO.java
@@ -2,6 +2,7 @@ package org.zeus.demo.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.zeus.demo.model.Behavior;
 import org.zeus.demo.model.Status;
 
 @Getter
@@ -9,10 +10,12 @@ public class BotStateDTO {
     private Long id;
     private String name;
     private Status status;
+    private Behavior behavior;
 
-    public BotStateDTO(Long id, String name, Status status) {
+    public BotStateDTO(Long id, String name, Status status, Behavior behavior) {
         this.id = id;
         this.name = name;
         this.status = status;
+        this.behavior = behavior;
     }
 }
